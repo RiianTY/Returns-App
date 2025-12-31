d5f022baca12ff27ef254d29e029624926adac48
Ryan Daynes <info@ryandaynes.uk>
2025-12-31 16:43:18 +0000

    v0.9.6 - Sign out icon and version number in header

--- src/DefaultLayout.tsx
+++ src/DefaultLayout.tsx
@@ -59,12 +59,14 @@ function DefaultLayout({ children }: { children: React.ReactNode }) {
             />
           </Button>
         </div>
-        <p>Version 0.9.5</p>
+        
         <h1 className="text-2xl font-bold">Returns App Beta</h1>
       </header>
       <main className="flex-1 p-4">{children}</main>
       <footer className="bg-gray-200 text-center p-4 ">
         &copy; {new Date().getFullYear()} Techra. All rights reserved.
+        <br />
+        Version 0.9.6
       </footer>
     </div>
   );
