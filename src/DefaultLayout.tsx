9d4d6f60ef312307be0381637faf6097297d035d
Ryan Daynes <info@ryandaynes.uk>
2025-12-31 14:55:51 +0000

    v0.9.4 - Version number in header

--- src/DefaultLayout.tsx
+++ src/DefaultLayout.tsx
@@ -2,6 +2,7 @@ function DefaultLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen flex flex-col">
       <header className="text-black p-4">
+        <p>0.9.4</p>
         <h1 className="text-2xl font-bold">Returns App Beta</h1>
       </header>
       <main className="flex-grow p-4">{children}</main>
